c1e6032ca26493cee29a471c5b9ffe49e0988c12
dgruncharov <damian.grancharov@gmail.com>
2015-06-22 20:37:41 +0300

    Methods

--- ConsoleApplication1/ConsoleApplication1/FileMethods.cs
+++ ConsoleApplication1/ConsoleApplication1/FileMethods.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class FileMetods
+    class FileMethods
     {
         public static List<SistemaBudjet> ReadFromFile(string filename, int days);
+
+        internal static void WriteFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ConsoleApplication1/ConsoleApplication1/Methods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Methods
+    {
+        private static List<SistemaBudjet> list;
+        private static string filename = string.Empty;
+
+        public static string Filename
+        {
+            get { return filename; }
+            set { filename = value; }
+        }
+        public static void AddRecord()
+        {
+            Console.WriteLine("Въведете дата: ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Въведете име на разход: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Въведете цена: ");
+            double price = double.Parse(Console.ReadLine());
+
+            SistemaBudjet entry = new SistemaBudjet(date, name, price);
+        }
+
+        private static double GetExpenses(int option)
+        {
+            List<SistemaBudjet> lastWeek = FileMethods.ReadFromFile(Filename, 7);
+            double result = 0;
+            foreach (SistemaBudjet item in lastWeek)
+            {
+                result += item.Price;
+            }
+            return result;
+
+        }
+
+        public static double LastWeekExpenses()
+        {
+            double result = GetExpenses(7);
+            return result;
+        }
+        public static double LastMonthExpenses()
+        {
+            double result = GetExpenses(30);
+            return result;
+        }
+        public static double LastYearExpenses()
+        {
+            double result = GetExpenses(365);
+            return result;
+        }
+
+        public static void Export()
+        {
+            Console.WriteLine("Въведете име на файл за запис: ");
+            string filename = Console.ReadLine();
+            FileMethods.WriteFile(filename);
+        }
+        public static void Import()
+        {
+            Console.WriteLine("Въведете име на файл за четене: ");
+            string filename = Console.ReadLine();
+            Filename = filename;
+        }
+    }
+}
