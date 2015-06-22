53246fa2f27dcc883d6ae153962055e9dee5c00e
madoxxs77 <simeon_pri@abv.bg>
2015-06-22 19:33:02 +0300

    tova e proekta!

---
+++ ConsoleApplication1/ConsoleApplication1/SistemaBudjet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class SistemaBudjet
+    {
+        private DateTime data;
+
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        private string razhod;
+
+        public string Razhod
+        {
+            get { return razhod; }
+            set { razhod = value; }
+        }
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public SistemaBudjet(DateTime date, string razhod, double price)
+        {
+            this.data = date;
+            this.razhod = razhod;
+            this.price = price;
+        }
+    }
+}
