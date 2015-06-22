0fc38f77bee80ed5919e64ec40a2e68dda838123
madoxxs77 <simeon_pri@abv.bg>
2015-06-22 20:11:48 +0300

    menu

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("");
+            int choice = int.Parse(Console.ReadLine());
+            while (choice != 7)
+            {
+                switch (choice)
+                {
+                    case 1: 
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5: 
+                    case 6:
+                    case 7: return;
+                    default: Console.WriteLine("Моля въведете цифра от 1 до 6!"); break;
+
+                }
+                choice = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
