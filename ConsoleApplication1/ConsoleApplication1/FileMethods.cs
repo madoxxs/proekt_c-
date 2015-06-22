a7421a4140bbaa5683336208140903d1a2dfec38
dgruncharov <damian.grancharov@gmail.com>
2015-06-22 21:32:05 +0300

    Methods

--- ConsoleApplication1/ConsoleApplication1/FileMethods.cs
+++ ConsoleApplication1/ConsoleApplication1/FileMethods.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
 {
     class FileMethods
     {
-        public static List<SistemaBudjet> ReadFile(string file, int day)
+        public static List<SistemaBudjet> ReadFile(string file, Options option)
         {
             StreamReader readFile = new StreamReader(file);
             string line;
@@ -24,8 +24,16 @@ namespace ConsoleApplication1
                     DateTime date = DateTime.Parse(lineMass[0]);
                     double cena = double.Parse(lineMass[2]);
                     SistemaBudjet temp = new SistemaBudjet(date, lineMass[1], cena);
-                    DateTime dnes = DateTime.Now;
-                    DateTime daysBefore = dnes.Subtract(TimeSpan.FromDays(day));
+                    DateTime today = DateTime.Now;
+                    DateTime daysBefore = new DateTime();
+                    if (option == Options.week)
+                    {
+                        daysBefore = today.Subtract(TimeSpan.FromDays(7));
+                    }
+                    else if (option == Options.month)
+                    {
+                        daysBefore = today.Subtract(TimeSpan.from)
+                    }
 
                     if (temp.Data <= dnes && temp.Data >= daysBefore)
                     {
